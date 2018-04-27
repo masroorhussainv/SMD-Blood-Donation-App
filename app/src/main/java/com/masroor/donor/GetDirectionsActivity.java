66cfa7688c735bd38307855b98a128440182b38c
masroor <masroorh7@gmail.com>
2018-04-27 05:36:10 +0500

    Change the way MapFragment is added to the activity, now add it dynamically instead of adding through a static xml file

--- app/src/main/java/com/masroor/donor/GetDirectionsActivity.java
+++ app/src/main/java/com/masroor/donor/GetDirectionsActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -16,7 +17,6 @@ import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.masroor.R;
 
 public class GetDirectionsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -26,13 +26,13 @@ public class GetDirectionsActivity extends FragmentActivity implements OnMapRead
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_get_directions);
+        setContentView(R.layout.activity_get_directions);
+
 
         if (checkPlayServices(this)) {
-            setContentView(R.layout.map_layout);
             initMapObject();
         } else {
-            setContentView(R.layout.activity_get_directions);
+            Toast.makeText(this, "Google Play Services not supported.", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -55,32 +55,28 @@ public class GetDirectionsActivity extends FragmentActivity implements OnMapRead
 
     public void initMapObject() {
         if (mMap == null) {
-            SupportMapFragment mapFragment =
-                    (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
-            if (mapFragment != null) {
-                mapFragment.getMapAsync(this);
-            } else {
-                Toast.makeText(getApplicationContext(), "Fargment manager error", Toast.LENGTH_LONG).show();
-            }
+//            SupportMapFragment mapFragment =
+//                    (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
+
+            MapFragment mapFragment=MapFragment.newInstance();
+
+            FragmentTransaction trans=getFragmentManager()
+                    .beginTransaction();
+                    trans.add(R.id.rootConstraintLayout,mapFragment);
+            trans.commit();
+
+            mapFragment.getMapAsync(this);
+
 
         }
     }
 
 
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
         mMap.setMyLocationEnabled(true);
         mMap.getUiSettings().setMyLocationButtonEnabled(true);
     }
