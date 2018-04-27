d981452a61d26bcce6f627d0ccec867b8369170a
masroor <masroorh7@gmail.com>
2018-04-27 05:17:23 +0500

    Add support for displaying the current location on the map in the 'GetDirectionactivity'

--- app/src/main/java/com/masroor/donor/GetDirectionsActivity.java
+++ app/src/main/java/com/masroor/donor/GetDirectionsActivity.java
@@ -1,7 +1,11 @@
 package com.masroor.donor;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,7 +19,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.masroor.R;
 
-public class GetDirectionsActivity extends FragmentActivity implements OnMapReadyCallback{
+public class GetDirectionsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     GoogleMap mMap;
 
@@ -24,10 +28,10 @@ public class GetDirectionsActivity extends FragmentActivity implements OnMapRead
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_get_directions);
 
-        if(checkPlayServices(this)){
+        if (checkPlayServices(this)) {
             setContentView(R.layout.map_layout);
             initMapObject();
-        }else{
+        } else {
             setContentView(R.layout.activity_get_directions);
         }
     }
@@ -40,23 +44,23 @@ public class GetDirectionsActivity extends FragmentActivity implements OnMapRead
             if (apiAvailability.isUserResolvableError(resultCode)) {
                 apiAvailability.getErrorDialog(activity, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST)
                         .show();
-                Toast.makeText(activity,"Device is supported for Google Play Services!",Toast.LENGTH_LONG).show();
+                Toast.makeText(activity, "Device is supported for Google Play Services!", Toast.LENGTH_LONG).show();
             } else {
-                Toast.makeText(activity,"Device not supported!",Toast.LENGTH_LONG).show();
+                Toast.makeText(activity, "Device not supported!", Toast.LENGTH_LONG).show();
             }
             return false;
         }
         return true;
     }
 
-    public void initMapObject(){
-        if(mMap==null){
-            SupportMapFragment mapFragment=
-                    (SupportMapFragment)  getSupportFragmentManager().findFragmentById(R.id.map_fragment);
-            if(mapFragment!=null){
+    public void initMapObject() {
+        if (mMap == null) {
+            SupportMapFragment mapFragment =
+                    (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
+            if (mapFragment != null) {
                 mapFragment.getMapAsync(this);
-            }else{
-                Toast.makeText(getApplicationContext(),"Fargment manager error",Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Fargment manager error", Toast.LENGTH_LONG).show();
             }
 
         }
@@ -65,7 +69,19 @@ public class GetDirectionsActivity extends FragmentActivity implements OnMapRead
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap=googleMap;
+        mMap = googleMap;
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
     }
 }
